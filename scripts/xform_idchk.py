ae8e4fa642726a3202c0e702bb5dda78ee0c906f
Stephen L Arnold <sarnold@vctlabs.com>
2024-11-26 14:40:29 -0800

    chg: dev: update idchk script to de-duplicate before sorting
    Signed-off-by: Stephen L Arnold <sarnold@vctlabs.com>

--- scripts/xform_idchk.py
+++ scripts/xform_idchk.py
@@ -1,5 +1,5 @@
 """
-Simple consumer test.
+Simple consumer test with de-dup and sort.
 """
 
 import os
@@ -17,11 +17,23 @@ OPTS = {
     'csv_delimiter': ';',
 }
 
+
+def set_unique(sequence):
+    """
+    Remove duplicates and emulate a set with ordered elements.
+    """
+    seen = set()
+    return [x for x in sequence if not (x in seen or seen.add(x))]
+
+
 # read in some json "column data"
 data = text_file_reader(FILE, OPTS)
-if data[0].isupper():
-    lc_ids = [xform_id(x) for x in data if len(x) > 0]
+
+# we assume input IDs are classic upper case
+raw_ids = [xform_id(x) for x in data if len(x) > 0]
+
+unique_ids = set_unique(raw_ids)
 
 # spit out lowercase id format
-for ctl in os_sorted(lc_ids):
+for ctl in os_sorted(unique_ids):
     print(ctl)
